46dc9c518399278e484a4db79a5d53b4c8a75164
DavidSanzMajolero <david.sanz.7e7@itb.cat>
2024-04-03 12:49:37 +0200

    All requirements implemented

--- Program.cs
+++ Program.cs
@@ -8,9 +8,11 @@ namespace AC1
     {
         public static void Main()
         {
-            int num = 1, playerScore;
-            string playerName, playerMission;
-            const string Player = "Player ", AskPlayer = "Write player's name", AskMission = "Write player's mission", AskScore = "Write player's score";
+            int num = 1, numMissions = 0;
+            string playerName;
+            string[] playerMissions;
+            int[] playerScores;
+            const string Player = "Player ", AskPlayer = "Write player's name", AskMission = "Write player's mission", AskScore = "Write player's score", AskNumMissions = "Write the number of missions";
             List<Score> players = new List<Score>();
 
             for (int i = 0; i < 2; i++)
@@ -23,21 +25,30 @@ namespace AC1
                     playerName = Console.ReadLine();
                 } while (!ValidatePlayer(playerName));
 
-                do
-                {
-                    Console.ForegroundColor = ConsoleColor.Blue;
-                    Console.WriteLine(AskMission);
-                    playerMission = Console.ReadLine();
-                } while (!ValidateMission(playerMission));
+                Console.ForegroundColor = ConsoleColor.Red;
+                Console.WriteLine(AskNumMissions);
+                numMissions = Convert.ToInt32(Console.ReadLine());
+                playerMissions = new string[numMissions];
+                playerScores = new int[numMissions];
 
-                do
+                for (int j = 0; j < numMissions; j++)
                 {
-                    Console.ForegroundColor = ConsoleColor.Yellow;
-                    Console.WriteLine(AskScore);
-                    playerScore = Convert.ToInt32(Console.ReadLine());
-                } while (!ValidateScore(playerScore));
+                    do
+                    {
+                        Console.ForegroundColor = ConsoleColor.Blue;
+                        Console.WriteLine(AskMission);
+                        playerMissions[j] = Console.ReadLine();
+                    } while (!ValidateMission(playerMissions[j]));
 
-                Score player = new Score(playerName, playerMission, playerScore);
+                    do
+                    {
+                        Console.ForegroundColor = ConsoleColor.Yellow;
+                        Console.WriteLine(AskScore);
+                        playerScores[j] = Convert.ToInt32(Console.ReadLine());
+                    } while (!ValidateScore(playerScores[j]));
+                }
+
+                Score player = new Score(playerName, playerMissions, playerScores);
                 players.Add(player);
 
                 num++;
@@ -47,23 +58,39 @@ namespace AC1
 
             List<Score> uniqueRanking = GenerateUniqueRanking(players);
 
+            Console.WriteLine("Unique Ranking:");
             foreach (Score player in uniqueRanking)
             {
                 Console.ForegroundColor = ConsoleColor.Green;
                 Console.WriteLine("Player: " + player.Player);
                 Console.ForegroundColor = ConsoleColor.Blue;
-                Console.WriteLine("Mission: " + player.Mission);
-                Console.ForegroundColor = ConsoleColor.Yellow;
-                Console.WriteLine("Score: " + player.Scores);
+                for (int i = 0; i < player.Missions.Length; i++)
+                {
+                    Console.WriteLine("Mission: " + player.Missions[i] + " Score: " + player.Scores[i]);
+                }
                 Console.WriteLine();
                 Console.ResetColor();
             }
+            Console.ReadLine();
+
+            List<Score> rankedScores = Score.RankingByScore(players);
+            Console.WriteLine("Ranking by Score:");
+            int top = 0;
+            foreach (Score score in rankedScores)
+            {
+                top++;
+                Console.ForegroundColor = ConsoleColor.Yellow;
+                Console.WriteLine($"TOP {top}: Player: {score.Player}, Mission: {score.Missions[0]}, Score: {score.Scores[0]}");
+            }
+            Console.ResetColor();
         }
+
         public static bool ValidatePlayer(string input)
         {
-            Regex regex = new Regex(@"[^a-zA-Z0-9\s]");
-            return !regex.IsMatch(input);
+            Regex regex = new Regex(@"^[a-zA-Z0-9\s]+$");
+            return regex.IsMatch(input);
         }
+
         public static bool ValidateMission(string input)
         {
             string[] GreeceAlph = { "Alfa", "Beta", "Gamma", "Delta", "Epsilon", "Zeta", "Eta", "Theta", "Iota", "Kappa", "Lambda", "Mi", "Ni", "Ksi", "Omicron", "Pi", "Ro", "Sigma", "Tau", "Ipsilon", "Fi", "Khi", "Psi", "Omega" };
@@ -80,17 +107,56 @@ namespace AC1
             string pattern = @"^.*-\d{3}$";
             return containsGreekWord && Regex.IsMatch(input, pattern);
         }
+
         public static bool ValidateScore(int num)
         {
-            if (num >= 0 && num <= 500)
-            {
-                return true;
-            }
-            else return false;
+            return num >= 0 && num <= 500;
         }
-        public static List<Score> GenerateUniqueRanking (List<Score> players)
+
+        public static List<Score> GenerateUniqueRanking(List<Score> players)
         {
-            players.Sort((x, y) => y.Scores.CompareTo(x.Scores));
+            for (int i = 0; i < players.Count; i++)
+            {
+                for (int j = 0; j < players[i].Missions.Length; j++)
+                {
+                    for (int k = j + 1; k < players[i].Missions.Length; k++)
+                    {
+                        if (players[i].Missions[j] == players[i].Missions[k])
+                        {
+                            if (players[i].Scores[j] > players[i].Scores[k])
+                            {
+                                players[i].Missions[k] = null;
+                                players[i].Scores[k] = 0;
+                            }
+                            else
+                            {
+                                players[i].Missions[j] = null;
+                                players[i].Scores[j] = 0;
+                            }
+                        }
+                    }
+                }
+            }
+
+            for (int i = 0; i < players.Count; i++)
+            {
+                List<string> validMissions = new List<string>();
+                List<int> validScores = new List<int>();
+
+                for (int j = 0; j < players[i].Missions.Length; j++)
+                {
+                    if (players[i].Missions[j] != null || players[i].Scores[j] != 0)
+                    {
+                        validMissions.Add(players[i].Missions[j]);
+                        validScores.Add(players[i].Scores[j]);
+                    }
+                }
+
+                players[i].Missions = validMissions.ToArray();
+                players[i].Scores = validScores.ToArray();
+            }
+
+            players.Sort((x, y) => y.TotalScore.CompareTo(x.TotalScore));
             return players;
         }
     }
